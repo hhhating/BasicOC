1fcdc1d04c1d6eeb9771ed829bda2fefde8a76ea
litingting.ltt <litingting.ltt@bytedance.com>
2023-07-13 17:39:10 +0800

    Initial Commit

---
+++ LogIn/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LogIn
+//
+//  Created by ByteDance on 2023/7/13.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
